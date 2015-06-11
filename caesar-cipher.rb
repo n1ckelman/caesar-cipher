4c3b9e2bd88a6d816964979b7ad8bf97e5f6ec96
n1ckelman <rybkin.k@gmail.com>
2015-06-12 00:07:32 +0300

    caesar's cipher v1.01

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,18 +1,17 @@
 def cesar(string, form)
     string = string.split("")
-	alph = ('a'..'z').to_a.join("")
-	result =[]
+    alph = ('a'..'z').to_a.join("")
+    result =[]
 
-	string.each do |letter|
+    string.each do |letter|
         if alph.include?(letter)
-    	    old_index = alph.index(letter)
-    	    new_index = -26+old_index+form
-    	    result.push(alph[new_index])
+            old_index = alph.index(letter)
+            result.push(alph[-26+old_index+form])
         else
             result.push(letter)
         end
-	end
+    end
     puts result.join("").capitalize
 end
 
-cesar("what a string!", 5)
+cesar("What a string!", 5)
